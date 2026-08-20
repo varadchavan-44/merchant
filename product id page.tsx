46b0df12a7f8b4fb718f370a5e2c0a9cba88ed2b
Varad Sambhaji Chavan <bt25cme048@students.vnit.ac.in>
2026-08-21 03:56:46 +0530

    Create product id page for displaying product details

---
+++ product id page.tsx
@@ -0,0 +1,23 @@
+import { notFound } from "next/navigation";
+import { SiteHeader } from "@/components/SiteHeader";
+import { ProductDetail } from "@/components/ProductDetail";
+import { getProduct } from "@/lib/data";
+
+export default async function PreviewProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = await getProduct(id);
+  if (!product) notFound();
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <SiteHeader />
+      <main className="flex-1">
+        <ProductDetail product={product} />
+      </main>
+    </div>
+  );
+}
