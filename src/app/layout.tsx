243cab128b3852b79561496541c3c2a53eb0a86e
Varad Sambhaji Chavan <bt25cme048@students.vnit.ac.in>
2026-08-21 13:55:01 +0530

    Add Footer component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "@fontsource/dm-sans/500.css";
 import "@fontsource/dm-sans/600.css";
 import "@fontsource/dm-sans/700.css";
 import "./globals.css";
+import { Footer } from "@/components/Footer";
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en" className={spaceGrotesk.variable}>
       <body className="antialiased">
         {children}
+        <Footer />
       </body>
     </html>
   );
